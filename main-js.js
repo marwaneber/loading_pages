e044261d6432fe1a5dd73067a096f41af5ce156e
Marwane Beriich <marber.mb11@gmail.com>
2016-11-22 21:31:44 +0000

    Update main-js.js

--- main-js.js
+++ main-js.js
@@ -7,14 +7,14 @@ var speed   = 4,
 var circle  = document.getElementsByClassName("circle");
 function inter(){
     for(var i =0;i<6;i++){
-        width   += speed ;
-        height  += speed ;
         circle[i].style.width   = "" + width + "px";
         circle[i].style.height  = "" + height + "px";
-        circle[i].style.background  = "rgb("+color*i+","+(255-color)+","+(color*i)+")";
+        circle[i].style.background  = "rgb("+color*i+","+(255-color*i)+","+(color*i)+")";
         circle[i].style.marginTop   = ""+(max-height)+"px";
-        if(y > max){speed = -speed;}
-        if(y < min){speed = -speed;} 
+        if(width > max){speed = -speed;}
+        if(width < min){speed = -speed;} 
+        width   += speed ;
+        height  += speed ;
     }
 }
 window.setInterval(inter,1000/3);
