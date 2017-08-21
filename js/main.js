f99ac531e35036f14b25aadad153f5a8fbc4872c
Marwane Beriich <marber.mb11@gmail.com>
2017-08-21 23:58:09 +0100

    adding responsive to home page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-
-	var count = 0;
-	var menu =  document.getElementsByClassName("list")[0];
-	function showMenu(){
-		if(count % 2 == 0){
-			menu.style.top = "0px";
-			menu.style.opacity = "0";
-		}else {
-			menu.style.top = "70px";
-			menu.style.opacity = "1";
-		}
-		count++;
+var count = 1;
+var menu =  document.getElementsByClassName("list")[0];
+function showMenu(){
+	if(count % 2 == 0){
+		menu.style.top = "0px";
+		menu.style.opacity = "0";
+	}else {
+		menu.style.top = "100px";
+		menu.style.opacity = "1";
 	}
+	count++;
+}
