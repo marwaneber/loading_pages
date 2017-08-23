dff6e655a1181c0e1d995b30f9039b8868a6187a
Marwane Beriich <marber.mb11@gmail.com>
2017-08-23 23:38:28 +0100

    adjusting home page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 var count = 1;
 var menu =  document.getElementsByClassName("list")[0];
+var container =  document.getElementsByClassName("container")[0];
 function showMenu(){
-	if(count % 2 == 0){
+	if(count % 2 != 0){
+		menu.style.top = "80px";
+		menu.style.opacity = "1";
+		container.style.margin = "150px auto 0 auto"
+	}else {
 		menu.style.top = "0px";
 		menu.style.opacity = "0";
-	}else {
-		menu.style.top = "100px";
-		menu.style.opacity = "1";
+		container.style.margin = "0 auto"
 	}
 	count++;
 }
